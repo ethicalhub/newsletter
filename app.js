fb7e5bc46ad70f37d3a1974e5849d57e1d194710
ethicalhub <yasiransari.elith@gmail.com>
2020-10-22 04:31:13 +0530

    first commit of signup, complete basic functionality of signup on newsletter and store all the data

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const https = require('https')
+const env = require('dotenv').config()
+const { response } = require('express')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/public'))
+
+app.get("/" , (req, res)=>{
+    res.sendFile(__dirname+ "/index.html")
+})
+
+
+app.post("/", (req,res)=>{
+    const fName = req.body.firstName
+    const lName = req.body.lastName
+    const email = req.body.email
+
+    const data = {
+        members : [
+            {
+                email_address : email,
+                status : "subscribed",
+                merge_fields : {
+                    FNAME : fName,
+                    LNAME : lName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    
+    const url = `https://us17.api.mailchimp.com/3.0/lists/${process.env.LIST_ID}`
+    const options = {
+        method : "POST",
+        auth : `yasir123:${process.env.API_KEY}`
+    }
+
+     const request = https.request(url , options, (response)=>{
+
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else {
+            res.sendFile(__dirname + "/failure.html")
+        }
+
+
+
+        response.on("data", (data)=>{
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData)
+    request.end()
+
+
+})
+
+app.post("/failure" , (req,res)=>{
+    res.redirect("/")
+})
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Server is up and running...");
+})
